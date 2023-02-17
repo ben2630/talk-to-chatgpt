c4bb15ea33100a5e47e32a11f04a874a3da03a5c
ben2630 <122989872+ben2630@users.noreply.github.com>
2023-02-16 22:02:22 -0600

    Updated content.js to use Google text-to-speech

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,133 +1,59 @@
-﻿// TALK TO CHATGPT
-// ---------------
-// Author		: C. NEDELCU
-// Version		: 1.6.1
-// Git repo 	: https://github.com/C-Nedelcu/talk-to-chatgpt
-// Chat GPT URL	: https://chat.openai.com/chat
-// How to use   : https://www.youtube.com/watch?v=VXkLQMEs3lA
-
-
-// ----------------------------
-// SETTINGS (FEEL FREE TO EDIT)
-// ----------------------------
-// These are the default settings. Since v1.3, a 'settings' menu allows to change most of the below values in the UI
-// Since v1.4, these settings are saved. So there is no need to edit them out anymore.
-
-// Settings for the text-to-speech functionality (the bot's voice)
-var CN_TEXT_TO_SPEECH_RATE = 1; // The higher the rate, the faster the bot will speak
-var CN_TEXT_TO_SPEECH_PITCH = 1; // This will alter the pitch for the bot's voice
-
-// Indicate a locale code such as 'fr-FR', 'en-US', to use a particular language for the speech recognition functionality (when you speak into the mic)
-// If you leave this blank, the system's default language will be used
-var CN_WANTED_LANGUAGE_SPEECH_REC = ""; //"fr-FR";
-
-// Determine which word will cause this scrip to stop.
-var CN_SAY_THIS_WORD_TO_STOP = "stop";
-
-// Determine which word will cause this script to temporarily pause
-var CN_SAY_THIS_WORD_TO_PAUSE = "pause";
-
-// Determine whether messages are sent immediately after speaing
-var CN_AUTO_SEND_AFTER_SPEAKING = true;
-
-// Determine which word(s) will cause this script to send the current message (if auto-send disabled)
-var CN_SAY_THIS_TO_SEND = "send message now"; 
-
-// Indicate "locale-voice name" (the possible values are difficult to determine, you should just ignore this and use the settings menu instead)
-var CN_WANTED_VOICE_NAME = "";
-
-// ----------------------------
-
-
-// -------------------
-// CODE (DO NOT ALTER)
-// -------------------
-var CN_MESSAGE_COUNT = 0;
-var CN_CURRENT_MESSAGE = null;
-var CN_CURRENT_MESSAGE_SENTENCES = [];
-var CN_CURRENT_MESSAGE_SENTENCES_NEXT_READ = 0;
-var CN_SPEECHREC = null;
-var CN_IS_READING = false;
-var CN_IS_LISTENING = false;
-var CN_FINISHED = false;
-var CN_PAUSED = false;
-var CN_WANTED_VOICE = null;
-var CN_TIMEOUT_KEEP_SYNTHESIS_WORKING = null;
-var CN_TIMEOUT_KEEP_SPEECHREC_WORKING = null;
-var CN_SPEECH_REC_SUPPORTED = false;
-var CN_SPEAKING_DISABLED = false;
-var CN_SPEECHREC_DISABLED = false;
-
-// This function will say the given text out loud using the browser's speech synthesis API
+// This function will say the given text out loud using the Google Cloud Text-to-Speech API
 function CN_SayOutLoud(text) {
-	if (!text || CN_SPEAKING_DISABLED) {
-		if (CN_SPEECH_REC_SUPPORTED && CN_SPEECHREC && !CN_IS_LISTENING && !CN_PAUSED && !CN_SPEECHREC_DISABLED) CN_SPEECHREC.start();
-		clearTimeout(CN_TIMEOUT_KEEP_SPEECHREC_WORKING);
-		CN_TIMEOUT_KEEP_SPEECHREC_WORKING = setTimeout(CN_KeepSpeechRecWorking, 100);
-		return;
-	}
-	
-	// Are we speaking?
-	if (CN_SPEECHREC) {
-		clearTimeout(CN_TIMEOUT_KEEP_SPEECHREC_WORKING);
-		CN_SPEECHREC.stop();
-	}
-	
-	// Let's speak out loud
-	console.log("Saying out loud: "+text);
-	var msg = new SpeechSynthesisUtterance();
-	msg.text = text;
-	
-	if (CN_WANTED_VOICE) msg.voice = CN_WANTED_VOICE;
-	msg.rate = CN_TEXT_TO_SPEECH_RATE;
-	msg.pitch = CN_TEXT_TO_SPEECH_PITCH;
-	msg.onstart = () => {
-		// Make border green
-		$("#TTGPTSettings").css("border-bottom", "8px solid green");
-		
-		// If speech recognition is active, disable it
-		if (CN_IS_LISTENING) CN_SPEECHREC.stop();
-		
-		if (CN_FINISHED) return;
-		CN_IS_READING = true;
-		clearTimeout(CN_TIMEOUT_KEEP_SYNTHESIS_WORKING);
-		CN_TIMEOUT_KEEP_SYNTHESIS_WORKING = setTimeout(CN_KeepSpeechSynthesisActive, 5000);
-	};
-	msg.onend = () => {
-		CN_AfterSpeakOutLoudFinished();
-	}
-	CN_IS_READING = true;
-	window.speechSynthesis.speak(msg);
-}
+    if (!text || CN_SPEAKING_DISABLED) {
+        if (CN_SPEECH_REC_SUPPORTED && CN_SPEECHREC && !CN_IS_LISTENING && !CN_PAUSED && !CN_SPEECHREC_DISABLED) CN_SPEECHREC.start();
+        clearTimeout(CN_TIMEOUT_KEEP_SPEECHREC_WORKING);
+        CN_TIMEOUT_KEEP_SPEECHREC_WORKING = setTimeout(CN_KeepSpeechRecWorking, 100);
+        return;
+    }
 
-// Occurs when speaking out loud is finished
-function CN_AfterSpeakOutLoudFinished() {
-	// Make border grey again
-	$("#TTGPTSettings").css("border", "2px solid #888");
-	
-	if (CN_FINISHED) return;
-	
-	// Finished speaking
-	clearTimeout(CN_TIMEOUT_KEEP_SYNTHESIS_WORKING);
-	console.log("Finished speaking out loud");
-	
-	// restart listening
-	CN_IS_READING = false;
-	setTimeout(function() {
-		if (!window.speechSynthesis.speaking) {
-			if (CN_SPEECH_REC_SUPPORTED && CN_SPEECHREC && !CN_IS_LISTENING && !CN_PAUSED && !CN_SPEECHREC_DISABLED) CN_SPEECHREC.start();
-			clearTimeout(CN_TIMEOUT_KEEP_SPEECHREC_WORKING);
-			CN_TIMEOUT_KEEP_SPEECHREC_WORKING = setTimeout(CN_KeepSpeechRecWorking, 100);
-		}
-	}, 500);
-}
+    // Are we speaking?
+    if (CN_SPEECHREC) {
+        clearTimeout(CN_TIMEOUT_KEEP_SPEECHREC_WORKING);
+        CN_SPEECHREC.stop();
+    }
 
-// This is a workaround for Chrome's bug in the speech synthesis API (https://stackoverflow.com/questions/21947730/chrome-speech-synthesis-with-longer-texts)
-function CN_KeepSpeechSynthesisActive() {
-	console.log("Keeping speech synthesis active...");
-	window.speechSynthesis.pause();
-	window.speechSynthesis.resume();
-	CN_TIMEOUT_KEEP_SYNTHESIS_WORKING = setTimeout(CN_KeepSpeechSynthesisActive, 5000);
+    console.log("Saying out loud: "+text);
+    var audio = new Audio();
+    audio.src = 'https://texttospeech.googleapis.com/v1/text:synthesize?key=YOUR_API_KEY';
+    audio.addEventListener('loadedmetadata', function() {
+        audio.play();
+        if (CN_WANTED_VOICE) audio.setSinkId(CN_WANTED_VOICE);
+    });
+    audio.addEventListener('error', function(e) {
+        console.log(e);
+    });
+
+    // request to the Google Cloud Text-to-Speech API
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://texttospeech.googleapis.com/v1/text:synthesize?key=YOUR_API_KEY', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var audioContent = xhr.response.audioContent;
+        audio.src = 'data:audio/wav;base64,' + audioContent;
+    };
+    xhr.onerror = function() {
+        console.log('Error making request to Google Cloud Text-to-Speech API');
+    };
+    xhr.send(JSON.stringify({
+        input: {
+            text: text
+        },
+        voice: {
+            languageCode: 'en-US', // specify your desired language
+            name: 'en-US-Wavenet-D', // specify your desired voice
+        },
+        audioConfig: {
+            audioEncoding: 'LINEAR16',
+            speakingRate: CN_TEXT_TO_SPEECH_RATE,
+            pitch: CN_TEXT_TO_SPEECH_PITCH
+        }
+    }));
+
+    if (CN_IS_LISTENING) CN_SPEECHREC.stop();
+    CN_IS_READING = true;
+    CN_TIMEOUT_KEEP_SYNTHESIS_WORKING = setTimeout(CN_KeepSpeechSynthesisActive, 5000);
 }
 
 // Split the text into sentences so the speech synthesis can start speaking as soon as possible
